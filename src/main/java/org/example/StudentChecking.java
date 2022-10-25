40e07f03c72cafc8186a3f54c49debe1f1046adc
Veronika <veronikacasado@icloud.com>
2022-10-25 20:25:31 +0200

    first commit

---
+++ src/main/java/org/example/StudentChecking.java
@@ -0,0 +1,54 @@
+package org.example;
+
+public class StudentChecking{
+    private static double penaltyFee = 40;
+    private double Balance = 350;
+    private Object creationDate;
+
+    public StudentChecking(double Balance) {
+        setBalance(Balance);
+}
+
+
+
+
+    public void setBalance(double Balance){
+        this.Balance = Balance;
+    }
+
+    public void creationDate(double creationDate){
+        this.creationDate = creationDate;{
+
+
+
+        }
+
+        Object balance = null;
+        if(Balance > 250 )
+            balance = balance;
+
+        else
+
+            throw new IllegalArgumentException("Balance can't be lower than 250");
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
